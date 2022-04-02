e8ee7da8d5d57975da16042dfd56ae1e0319ba5d
Victor Aparecido Barbato Rossi <victorap93@gmail.com>
2022-04-02 11:35:54 -0300

    Reformulação da biblioteca e adição de conteudo explicativo.

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-readjson
+# ReadJson
+
+[![Latest Version](https://img.shields.io/github/release/victorap93/readjson.svg?style=flat-square)](https://github.com/victorap93/readjson/releases)
+[![Total Downloads](https://img.shields.io/packagist/dt/victorap93/readjson.svg?style=flat-square)](https://packagist.org/packages/victorap93/readjson)
+
+PowerBIEmbedded was created to facilitate the obtention of the necessary token to build its interface.
+
+
+## Installing ReadJson
+
+The recommended way to install this is through
+[Composer](https://getcomposer.org/).
+
+```bash
+composer require victorap93/readjson
+```
+
+
+## How to use ReadJson
+
+Assuming you have a *.json* file `./readjson/stores.json` with the following content.
+```json 
+{
+    "stores": [
+        {
+            "name": "store one",
+            "products": [
+                {
+                    "name": "product one",
+                    "amount": 10
+                },
+                {
+                    "name": "product two",
+                    "amount": 20
+                }
+            ]
+        },
+        {
+            "name": "store two",
+            "products": [
+                {
+                    "name": "product two",
+                    "amount": 22
+                },
+                {
+                    "name": "product three",
+                    "amount": 33
+                }
+            ]
+        }
+    ]
+}
+```
+
+And this *.php* in the same folder `./readjson/index.php`, the example below show different ways to acess *.json* file.
+```php
+use \victorap93\ReadJson;
+
+$json_path = "./stores.json";
+
+// Get all json content.
+$ReadJson = new ReadJson($json_path);
+$json_object_value1 = $ReadJson->getJsonObject();
+
+// Accessing the obtained object.
+$json_object_value2 = $json_object_value1->stores[0]->products;
+
+// Get a specified json content position in instance of class.
+$ReadJson = new ReadJson($json_path, ['stores', 0, 'products']);
+$json_object_value3 = $ReadJson->getJsonObject();
+
+// Get a specified json content position in method call.
+$ReadJson = new ReadJson($json_path, ['stores', 0]);
+$json_object_value4 = $ReadJson->getJsonObject(['products']);
+
+// Get a specified json content position with object.
+$json_object_value5 = $ReadJson->accessRecursiveKeys($json_object_value4, [0, 'name']);
+```
+
+
+## Help and docs
+
+- [JSON](https://www.json.org/json-en.html)
+
+
+## License
+
+PowerBIEmbedded is made available under the MIT License (MIT). Please see [License File](LICENSE) for more information.
--- src/ReadJson.php
+++ src/ReadJson.php
@@ -1,66 +1,44 @@
 <?php
 
-namespace Victorap93;
+namespace victorap93;
 
-class ReadJson{
+class ReadJson
+{
+    private $json_object;
 
-    private $path = "";
-    private $json_str = null;
-    private $default_indexes = [];
-
-    function __construct($path = "") {
-        if(!empty($path))
-            $this->set_path($path);
-    }
-
-    public function set_path($path)
+    public function __construct($json_path, $default_keys = [])
     {
-        if(is_file($path) && pathinfo($path)["extension"] == "json")
-            if(file_exists($path)){
-                $this->path = str_replace(['\\', '/', '\\\\', '//', '\\/', '\\/\\/'], DIRECTORY_SEPARATOR, $path);
-                $json_file = file_get_contents($this->path);
-                $this->json_str = json_decode($json_file, true);            
-            }else
-                throw new Exception("File not found.");
-        else
-            throw new Exception("Not a json file.");
-    }
-
-    public function get_path()
-    {
-        return $this->path;
-    }
-
-    public function set_default_index()
-    {
-        $this->default_indexes = func_get_args();
-    }
-    
-    public function get_default_index()
-    {
-        return $this->default_indexes;
-    }
-
-    public function get_json_str()
-    {
-        $json_str = $this->get_default_json_str();
-        $indexes = func_get_args();
-        return $this->access_recursive_index($json_str, $indexes);
-    }
-
-    private function get_default_json_str()
-    {
-        return $this->access_recursive_index($this->json_str, $this->default_indexes);
-    }
-
-    private function access_recursive_index($json_str, $indexes)
-    {
-        foreach($indexes as $index){
-            if(array_key_exists($index, $json_str))
-                $json_str = $json_str[$index];
-            else
-                throw new Exception("Index does not exist in json.");
+        if (is_file($json_path) && file_exists($json_path)) {
+            $json_object = $this->getJsonContent($json_path);
+            $this->json_object = $this->accessRecursiveKeys($json_object, $default_keys);
+        } else {
+            throw new \Exception("Can't find \"$json_path\" file.");
         }
-        return $json_str;
-    }    
+    }
+
+    private function getJsonContent($json_path)
+    {
+        if ($json_object = json_decode(file_get_contents($json_path))) {
+            return $json_object;
+        } else {
+            throw new \Exception("The file \"$json_path\" is not a valid json.");
+        }
+    }
+
+    public function accessRecursiveKeys($json_object, $keys)
+    {
+        foreach ($keys as $key) {
+            if (array_key_exists($key, $json_object)) {
+                $json_object = is_array($json_object) ? $json_object[$key] : $json_object->$key;
+            } else {
+                throw new \Exception("Key \"$key\" does not exist in specified json position.");
+            }
+        }
+        return $json_object;
+    }
+
+    public function getJsonObject($keys = [])
+    {
+        return $this->accessRecursiveKeys($this->json_object, $keys);
+    }
 }
